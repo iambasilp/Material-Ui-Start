fbcaa51b8e362da08a6b9a19cca03c5fa9afcd9b
iambasilp <basilxg321@gmail.com>
2022-12-17 21:29:52 +0530

    start with material ui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Typography, AppBar, Card, CardActions, CardMedia, CssBaseline, Grid, Toolbar, Container} from '@mui/material'
+import  PhotoCamera  from '@mui/icons-material';
+
+const App = ()=>{
+    return (
+        <>
+            <h1>Hello testing testin....</h1>                
+        </>
+    )
+}
+export default App;
